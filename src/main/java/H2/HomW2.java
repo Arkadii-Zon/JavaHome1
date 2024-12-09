c04b9de1ec45d36ad23e12eeb65ddd9d32d5eadb
bigri <bigrizon00@gmail.com>
2024-12-09 23:43:16 +0300

    2

--- src/main/java/H2/HomW2.java
+++ src/main/java/H2/HomW2.java
@@ -1,8 +1,8 @@
-package org.asd;
+package H2;
 
 import java.util.Arrays;
 
-public class H2 {
+public class HomW2 {
     // 1. Найти сумму элементов массива целых чисел.
     public static void task1() {
         int[] array = {1, 2, 3, 4};
@@ -207,6 +207,9 @@ public class H2 {
     public static void task14() {
         int[] array1 = {1, 2, 3};
         int[] array2 = {4, 5, 6};
+        for (int i = 0; i < array1.length; i++) {
+
+        }
     }
 
     // 15. Найти уникальные значения в массиве строк.
@@ -241,4 +244,58 @@ public class H2 {
     public static void task20() {
         String[] array = {"apple", "banana", "cherry"};
     }
+    // 21. Генерация нового массива из четных чисел.
+    public static void task21() {
+        int[] array = {1, 2, 3, 4, 5, 6};
+    }
+
+    // 22. Генерация нового массива из нечетных чисел.
+    public static void task22() {
+        int[] array = {1, 2, 3, 4, 5, 6};
+    }
+
+    // 23. Проверка на палиндром в массиве строк.
+    public static void task23() {
+        String[] array = {"level", "hello", "radar", "world"};
+    }
+
+    // 24. Поиск всех подстрок в строках массива, соответствующих заданному шаблону.
+    public static void task24() {
+        String[] array = {"apple", "application", "banana", "grape"};
+        String pattern = "app";
+    }
+
+    // 25. Создание массива чисел с четными индексами.
+    public static void task25() {
+        int[] array = {1, 2, 3, 4, 5, 6};
+    }
+
+    // 26. Создание массива чисел с нечетными индексами.
+    public static void task26() {
+        int[] array = {1, 2, 3, 4, 5, 6};
+
+    }
+
+    // 27. Разделение массива строк на два: строки с четной и нечетной длиной.
+    public static void task27() {
+        String[] array = {"apple", "banana", "cat", "dog", "elephant"};
+    }
+
+    // 28. Найти все строки, которые начинаются и заканчиваются одинаковой буквой.
+    public static void task28() {
+        String[] array = {"apple", "banana", "level", "radar", "hello"};
+
+    }
+
+    // 29. Создать массив чисел, которые являются квадратами элементов исходного массива.
+    public static void task29() {
+        int[] array = {1, 2, 3, 4, 5};
+
+    }
+
+    // 30. Создать массив строк, содержащих только уникальные символы.
+    public static void task30() {
+        String[] array = {"apple", "banana", "cat", "dog", "race"};
+    }
+
 }

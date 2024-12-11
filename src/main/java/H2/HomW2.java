6e25295abcf9e6c5da6454ebbff621412e4a087f
bigri <bigrizon00@gmail.com>
2024-12-11 17:22:50 +0300

    home2

--- src/main/java/H2/HomW2.java
+++ src/main/java/H2/HomW2.java
@@ -92,7 +92,7 @@ public class HomW2 {
         int[] array = {3, 1, 4, 1, 5};
         int min = array[0];
         for (int i = 0; i < array.length; i++) {
-            if (min > array[i]){
+            if (min > array[i]) {
                 min = array[i];
             }
         }
@@ -104,8 +104,8 @@ public class HomW2 {
     public static void task8() {
         String[] array = {"cat", "elephant", "dog"};
         String min = array[0];
-        for (int i = 0; i < array.length ; i++) {
-            if (min.length() > array[i].length() ){
+        for (int i = 0; i < array.length; i++) {
+            if (min.length() > array[i].length()) {
                 min = array[i];
             }
         }
@@ -118,7 +118,7 @@ public class HomW2 {
         int[] array = {1, 2, 3, 4, 5};
         int count = 0;
         for (int i = 0; i < array.length; i++) {
-            if (array[i] % 2 == 0){
+            if (array[i] % 2 == 0) {
                 count++;
             }
         }
@@ -147,27 +147,27 @@ public class HomW2 {
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
-                if (array[i].length() < array[j].length()){
+                if (array[i].length() < array[j].length()) {
                     min = array[i];
                 }
             }
-            a2 [0] = min;
+            a2[0] = min;
         }
-        for (int i = 0; i < array.length-1; i++) {
-            if (array[i].length() <= min.length()){
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i].length() <= min.length()) {
                 min1 = array[i];
             }
 
-            a2 [1] = min1;
+            a2[1] = min1;
         }
 
         for (int i = 2; i < array.length; i++) {
             for (int j = 2; j < array.length; j++) {
-                if (array[i].length() > min1.length()){
+                if (array[i].length() > min1.length()) {
                     min2 = array[i];
                 }
             }
-            a2 [2] = min2;
+            a2[2] = min2;
         }
         System.out.println(Arrays.toString(a2));
     }
@@ -175,24 +175,32 @@ public class HomW2 {
     // 12. Найти и вывести все строки одинаковой длины.
     public static void task12() {
         String[] array = {"apple", "banana", "cherry", "dog", "cat"};
-        String l1 = array[0];
-        String l2 = array[0];
-        String l3 = array[0];
-        String l4 = array[0];
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - 1; j++) {
-                if (array[i].length() > l1.length() && array[i].length() == array[j].length()){
-                    l1 = array[i];
-                    l2 = array[j];
-                }
-                if (array[i].length() < l1.length() && array[i].length() == array[j].length()) {
-                    l3 = array[i];
-                    l4 = array[j];
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i].length() == array[j].length()) {
+                    System.out.println("Строки одинаковой длины: " + array[i] + " и " + array[j]);
                 }
             }
         }
-        System.out.println("Строки одинаковой длины " + l1 + ", " + l2);
-        System.out.println("Строки одинаковой длины " + l3 + ", " + l4);
+//        String l1 = array[0];
+//        String l2 = array[0];
+//        String l3 = array[0];
+//        String l4 = array[0];
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array.length - 1; j++) {
+//                if (array[i].length() > l1.length() && array[i].length() == array[j].length()){
+//                    l1 = array[i];
+//                    l2 = array[j];
+//                }
+//                if (array[i].length() < l1.length() && array[i].length() == array[j].length()) {
+//                    l3 = array[i];
+//                    l4 = array[j];
+//                }
+//            }
+//        }
+//        System.out.println("Строки одинаковой длины " + l1 + ", " + l2);
+//        System.out.println("Строки одинаковой длины " + l3 + ", " + l4);
+        // данное решение не подошло, не выводит cherry
     }
 
     // 13. Сравнить содержимое двух массивов целых чисел.
@@ -207,43 +215,127 @@ public class HomW2 {
     public static void task14() {
         int[] array1 = {1, 2, 3};
         int[] array2 = {4, 5, 6};
+        int[] newArray = new int[array1.length];
         for (int i = 0; i < array1.length; i++) {
-
+            newArray[i] = array1[i] + array2[i];
         }
+        System.out.println(Arrays.toString(newArray));
     }
+
 
     // 15. Найти уникальные значения в массиве строк.
     public static void task15() {
         String[] array = {"apple", "banana", "apple", "cherry", "banana"};
+        boolean dubl = true;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i].equals(array[j]) && i != j) {
+                    dubl = false;
+                    break;
+                }
+            }
+            if (dubl) {
+                System.out.println(array[i]);
+            }
+
+        }
 
     }
 
     // 16. Удалить дубликаты из массива строк.
     public static void task16() {
         String[] array = {"apple", "banana", "apple", "cherry", "banana"};
+        boolean dubl = false;
+        int count = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (array[i].equals(array[j])) {
+                    dubl = true;
+                    break;
+                }
+            }
+            if (!dubl) {
+                count++;
+            }
+            dubl = false;
+
+        }
+        String[] a1 = new String[count];
+        int k = 0;
+        for (int i = 0; i < array.length; i++) {
+            dubl = false;
+            for (int j = i - 1; j >= 0; j--) {
+                if (array[i].equals(array[j])) {
+                    dubl = true;
+                    break;
+                }
+            }
+            if (!dubl) {
+                a1[k] = array[i];
+                k++;
+            }
+        }
+
+        System.out.println("arrayWithoutDublicates = " + Arrays.toString(a1));
     }
 
     // 17. Объединить два массива строк в один.
     public static void task17() {
         String[] array1 = {"apple", "banana"};
         String[] array2 = {"cherry", "date"};
+        String[] array3 = new String[array1.length + array2.length];
+        int j = 0;
+        for (int i = 0; i < array1.length; i++) {
+            array3[j] = array1[i];
+            j++;
+        }
+
+        for (int i = 0; i < array2.length; i++) {
+            array3[j] = array2[i];
+            j++;
+        }
+        System.out.println(Arrays.toString(array3));
     }
 
     // 18. Проверить, упорядочен ли массив строк по алфавиту.
     public static void task18() {
         String[] array = {"apple", "banana", "cherry"};
+        boolean isSorted = true;
+
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i].compareTo(array[i + 1]) > 0) {
+                isSorted = false;
+                break;
+            }
+        }
+
+        System.out.println("Массив упорядочен: " + isSorted);
     }
 
     // 19. Найти строки, начинающиеся с заданной буквы.
     public static void task19() {
         String[] array = {"apple", "banana", "apricot", "cherry"};
         char target = 'a';
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].charAt(0) == target) {
+                System.out.println(array[i]);
+            }
+        }
     }
 
     // 20. Создать массив строк из первых букв элементов другого массива.
     public static void task20() {
         String[] array = {"apple", "banana", "cherry"};
+        String[] array2 = new String[array.length];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length() > 0){
+                array2[i] = String.valueOf(array[i].charAt(0));
+            }
+        }
+        System.out.println(Arrays.toString(array2));
     }
+
     // 21. Генерация нового массива из четных чисел.
     public static void task21() {
         int[] array = {1, 2, 3, 4, 5, 6};
@@ -296,6 +388,45 @@ public class HomW2 {
     // 30. Создать массив строк, содержащих только уникальные символы.
     public static void task30() {
         String[] array = {"apple", "banana", "cat", "dog", "race"};
+        String allWords = array[0];
+
+        for (int i = 1; i < array.length; i++) {
+            allWords += array[i];
+        }
+
+        System.out.println("allWords = " + allWords);
+
+        boolean isUnique = true;
+        int countUniqueLetters = 0;
+        for (int i = 0; i < allWords.length(); i++) {
+            for (int j = 1; i >= j; j++) {
+                if (allWords.charAt(i) == allWords.charAt(i - j)) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) {
+                countUniqueLetters++;
+            }
+            isUnique = true;
+        }
+
+        String[] arrayUniqueLetters = new String[countUniqueLetters];
+        int k = 0;
+        for (int i = 0; i < allWords.length(); i++) {
+            isUnique = true;
+            for (int j = 1; i >= j; j++) {
+                if (allWords.charAt(i) == allWords.charAt(i - j)) {
+                    isUnique = false;
+                    break;
+                }
+            }
+            if (isUnique) {
+                arrayUniqueLetters[k] = String.valueOf(allWords.charAt(i));
+                k++;
+            }
+        }
+        System.out.println("arrayUniqueLetters = " + Arrays.toString(arrayUniqueLetters));
     }
 
 }
